d97868cd0855158d117fbaf7222f1fbccaa163ed
choheewon <choheewon@hanmail.net>
2026-01-05 17:55:44 +0900

    2일차

--- ex02/src/App.js
+++ ex02/src/App.js
@@ -6,10 +6,12 @@ import Quiz01Con from './containers/Quiz01Con';
 import Quiz02Con from './containers/Quiz02Con';
 import Ex02Con from './containers/Ex02Con';
 import Ex03Con from './containers/Ex03Con';
+import Quiz03Con from './containers/Quiz03Con';
 function App() {
   return (<>
-  <Ex03Con />
+  <Quiz03Con />
   {/*
+  <Ex03Con />
     <Ex02Con />
     <hr></hr>
     <Quiz02Con />
