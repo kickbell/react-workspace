d12ca7e23eb3bbd12694b5196d4ea38aa6ac5419
choheewon <choheewon@hanmail.net>
2026-01-08 16:23:52 +0900

    4일차

--- ex04_form_hooks/src/App.js
+++ ex04_form_hooks/src/App.js
@@ -1,7 +1,12 @@
+import CheckBox03Con from "./containers/CheckBox03Con";
 import Input01Con from "./containers/Input01Con";
+import Input02Con from "./containers/Input02Con";
 function App() {
   return (
     <>
+    <CheckBox03Con />
+    <hr></hr>
+    <Input02Con />
       <Input01Con />
     </>
   );
