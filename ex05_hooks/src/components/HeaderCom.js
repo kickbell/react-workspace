21619b5d8df4d7e8492a8b0330a0d81bcfb63f2b
choheewon <choheewon@hanmail.net>
2026-01-13 10:16:47 +0900

    7일차

--- ex05_hooks/src/components/HeaderCom.js
+++ ex05_hooks/src/components/HeaderCom.js
@@ -5,6 +5,7 @@ const HeaderCom = () => {
         <Link to="/list">목록</Link> &nbsp;
         <Link to="/login">로그인</Link> &nbsp;
         <Link to="/register">회원가입</Link> &nbsp;
+        <Link to="/context">context</Link> &nbsp;
         <hr></hr>
     </>)
 }
